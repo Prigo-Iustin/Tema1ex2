64bd157fa089f8133d4421f8de3ac202f37ea376
Prigo-Iustin <iustinian.prigoreanu@student.tuiasi.ro>
2026-02-22 23:08:43 +0200

    feat: Eliminare semne de punctuatie

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import string
+
 def citeste_fisier(nume_fisier):
     with open(nume_fisier, 'r') as f:
         return f.read()
 
+def elimina_punctuatie(text):
+    return text.translate(str.maketrans('', '', string.punctuation))
+
 text = citeste_fisier('input.txt')
+text_procesat = elimina_punctuatie(text)
