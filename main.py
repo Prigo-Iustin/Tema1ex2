b76159f3bba6c424f9a2d7b311c5a6e0eb981f52
Prigo-Iustin <iustinian.prigoreanu@student.tuiasi.ro>
2026-02-22 23:10:51 +0200

    feat: Eliminare spatii multiple si afisare la consola

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import string
+import re
 
 def citeste_fisier(nume_fisier):
     with open(nume_fisier, 'r') as f:
@@ -10,6 +11,14 @@ def elimina_punctuatie(text):
 def transforma_lowercase(text):
     return text.lower()
 
+def elimina_spatii_multiple(text):
+    return re.sub(r'\s+', ' ', text).strip()
+
+# Fluxul de executie
 text = citeste_fisier('input.txt')
 text_procesat = elimina_punctuatie(text)
 text_procesat = transforma_lowercase(text_procesat)
+text_final = elimina_spatii_multiple(text_procesat)
+
+# Afisare la consola
+print(text_final)
